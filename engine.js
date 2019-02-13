14960af20c44f2b2619d799bb49ea72b703d2567
Kousilla <rkarpov90@gmail.com>
2019-02-13 06:59:09 -0500

    add second row of flower platforms

--- engine.js
+++ engine.js
@@ -25,13 +25,36 @@ const purpleFlower2 = new Enemy(image, [2229], 74, 60, 52, -700, 230, 60, 52, 2)
 const whiteFlower5 = new Enemy(image, [2106], 138, 60, 52, -850, 230, 60, 52, 2)
 const whiteFlower6 = new Enemy(image, [2106], 138, 60, 52, -900, 230, 60, 52, 2)
 
-const tadpole1 = new Platform(image, 546, [911, 937, 963], 40, 23, -100, 200, 40, 23, 3, 5)
-const tadpole2 = new Platform(image, 546, [911, 937, 963], 40, 23, -50, 200, 40, 23, 3, 5)
-const tadpole3 = new Platform(image, 546, [911, 937, 963], 40, 23, -350, 200, 40, 23, 3, 5)
-const tadpole4 = new Platform(image, 546, [911, 937, 963], 40, 23, -650, 200, 40, 23, 3, 5)
-const tadpole5 = new Platform(image, 546, [911, 937, 963], 40, 23, -700, 200, 40, 23, 3, 5)
-const tadpole6 = new Platform(image, 546, [911, 937, 963], 40, 23, -1000, 200, 40, 23, 3, 5)
+const wogpole1 = new Platform(image, 546, [911, 937, 963], 40, 23, -125, 200, 40, 23, 3, 5)
+const wogpole2 = new Platform(image, 546, [911, 937, 963], 40, 23, -25, 200, 40, 23, 3, 5)
+const wogpole9 = new Platform(image, 546, [911, 937, 963], 40, 23, -75, 200, 40, 23, 3, 5)
 
+const wogpole3 = new Platform(image, 546, [911, 937, 963], 40, 23, -375, 200, 40, 23, 3, 5)
+const wogpole7 = new Platform(image, 546, [911, 937, 963], 40, 23, -325, 200, 40, 23, 3, 5)
+const wogpole8 = new Platform(image, 546, [911, 937, 963], 40, 23, -425, 200, 40, 23, 3, 5)
+
+const wogpole4 = new Platform(image, 546, [911, 937, 963], 40, 23, -625, 200, 40, 23, 3, 5)
+const wogpole5 = new Platform(image, 546, [911, 937, 963], 40, 23, -675, 200, 40, 23, 3, 5)
+const wogpole6 = new Platform(image, 546, [911, 937, 963], 40, 23, -725, 200, 40, 23, 3, 5)
+
+// const wollywog1 = new Platform(image, 840, [798], 47, 50, -100, 150, 47, 50, 1)
+const row2Flower1 = new Enemy(image, [2229], 74, 60, 52, -150, 145, 60, 52, 5)
+const row2Flower2 = new Enemy(image, [2229], 14, 60, 52, -200, 145, 60, 52, 5) // yellow
+// const row2Flower2 = new Enemy(image, [2106], 138, 60, 52, -200, 145, 60, 52, 5) // purple
+const row2Flower3 = new Enemy(image, [2229], 74, 60, 52, -250, 145, 60, 52, 5)
+// const row2Flower2 = new Enemy(image, [2109], 71, 60, 52, -200, 145, 60, 52, 5)
+// const row2Flower3 = new Enemy(image, [2106], 138, 60, 52, -250, 145, 60, 52, 5)
+const row2Flower4 = new Enemy(image, [2229], 74, 60, 52, -450, 145, 60, 52, 5)
+const row2Flower5 = new Enemy(image, [2229], 138, 60, 52, -500, 145, 60, 52, 5)
+const row2Flower6 = new Enemy(image, [2229], 138, 60, 52, -550, 145, 60, 52, 5)
+const row2Flower7 = new Enemy(image, [2229], 74, 60, 52, -700, 145, 60, 52, 5)
+const row2Flower8 = new Enemy(image, [2229], 138, 60, 52, -850, 145, 60, 52, 5)
+const row2Flower9 = new Enemy(image, [2229], 138, 60, 52, -900, 145, 60, 52, 5)
+
+
+// const wollywogImage = new Image();
+// wollywogImage.src = './assets/images/wollywog_sprites.png'
+// const wollywog1 = new Wog(wollywogImage, [596, 522], [858, 832], 47, 50, -100, 150, 47, 50, 3, 6)
 
 window.addEventListener("keydown", movePlayer, false);
 
@@ -80,12 +103,26 @@ function draw() {
     whiteFlower5.drawEnemy();
     whiteFlower6.drawEnemy();
 
-    tadpole1.drawPlatform();
-    // tadpole2.drawPlatform();
-    tadpole3.drawPlatform();
-    tadpole4.drawPlatform();
-    tadpole5.drawPlatform();
-    // tadpole6.drawPlatform();
+    wogpole1.drawPlatform();
+    wogpole2.drawPlatform();
+    wogpole3.drawPlatform();
+    wogpole4.drawPlatform();
+    wogpole5.drawPlatform();
+    wogpole6.drawPlatform();
+    wogpole7.drawPlatform();
+    wogpole8.drawPlatform();
+    wogpole9.drawPlatform();
+
+    row2Flower1.drawEnemy();
+    row2Flower2.drawEnemy();
+    row2Flower3.drawEnemy();
+    row2Flower4.drawEnemy();
+    row2Flower5.drawEnemy();
+    row2Flower6.drawEnemy();
+    row2Flower7.drawEnemy();
+    row2Flower8.drawEnemy();
+
+    // wollywog1.drawPlatform();
 
     window.requestAnimationFrame(draw);
 }
